49c30a709f8d37f6b5f71feaa0a4d1cfa6dcb13e
Maikoncarlos <maikonsaturno@gmail.com>
2024-02-02 19:27:14 -0300

    criação da camada de service e repository para autenticação do usuario

---
+++ src/main/java/com/github/maikoncarlos/clinicamedicaalura/service/AutenticacaoUsuarioService.java
@@ -0,0 +1,20 @@
+package com.github.maikoncarlos.clinicamedicaalura.service;
+
+import com.github.maikoncarlos.clinicamedicaalura.repository.usuario.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutenticacaoUsuarioService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(login);
+    }
+}
