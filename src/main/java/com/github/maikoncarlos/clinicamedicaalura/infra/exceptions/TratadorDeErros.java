d0f618d93645aa10a5862627ed0c44a825e570a6
Maikoncarlos <maikonsaturno@gmail.com>
2024-02-07 21:21:48 -0300

    devolvendo mensagem de erro na exception EntityNotFoundException e atualizando a msg de erro da DataIntegrityViolationException

--- src/main/java/com/github/maikoncarlos/clinicamedicaalura/infra/exceptions/TratadorDeErros.java
+++ src/main/java/com/github/maikoncarlos/clinicamedicaalura/infra/exceptions/TratadorDeErros.java
@@ -2,6 +2,7 @@ package com.github.maikoncarlos.clinicamedicaalura.infra.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,8 +15,9 @@ import java.util.List;
 public class TratadorDeErros {
 
     @ExceptionHandler (EntityNotFoundException.class)
-    public ResponseEntity<Void> tratarNotFoundException() {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<String> tratarNotFoundException() {
+        var error = "id não encontrado em nossa base!";
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
     @ExceptionHandler (MethodArgumentNotValidException.class)
@@ -32,8 +34,8 @@ public class TratadorDeErros {
     }
 
     @ExceptionHandler (DataIntegrityViolationException.class)
-    public ResponseEntity<String> tratarNotValidException(DataIntegrityViolationException exception) {
-        var error = "Já temos um cadastro com este email!";
+    public ResponseEntity<String> tratarViolationException(DataIntegrityViolationException exception) {
+        var error = "email já cadastrado!";
         return ResponseEntity.badRequest().body(error);
     }
 
