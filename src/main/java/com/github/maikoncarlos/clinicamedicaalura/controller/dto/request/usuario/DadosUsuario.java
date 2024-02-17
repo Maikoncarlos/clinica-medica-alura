50b3473f366f58eb94ef88357a2457a0618d801d
Maikoncarlos <maikonsaturno@gmail.com>
2024-02-16 22:39:29 -0300

    inserindo validações os dados de usuario para login e se login já exite na base

--- src/main/java/com/github/maikoncarlos/clinicamedicaalura/controller/dto/request/usuario/DadosUsuario.java
+++ src/main/java/com/github/maikoncarlos/clinicamedicaalura/controller/dto/request/usuario/DadosUsuario.java
@@ -1,6 +1,14 @@
 package com.github.maikoncarlos.clinicamedicaalura.controller.dto.request.usuario;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record DadosUsuario(
+        @NotBlank
+        @Email
         String login,
+        @NotBlank
+        @Size(min = 1, max = 8)
         String senha) {
 }
--- src/main/java/com/github/maikoncarlos/clinicamedicaalura/service/UsuarioService.java
+++ src/main/java/com/github/maikoncarlos/clinicamedicaalura/service/UsuarioService.java
@@ -3,6 +3,7 @@ package com.github.maikoncarlos.clinicamedicaalura.service;
 import com.github.maikoncarlos.clinicamedicaalura.controller.dto.request.usuario.DadosUsuario;
 import com.github.maikoncarlos.clinicamedicaalura.repository.usuario.UsuarioRepository;
 import com.github.maikoncarlos.clinicamedicaalura.service.mapper.ClinicaMapper;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,11 +17,20 @@ public class UsuarioService {
     private final PasswordEncoder passwordEncoder;
 
     public void execute(DadosUsuario dadosUsuario) {
+        validandoSeUsuarioJaCadastrada(dadosUsuario.login());
+
         var senhaEncriptada = passwordEncoder.encode(dadosUsuario.senha());
         var dadosUsuarioEncriptado = new DadosUsuario(dadosUsuario.login(), senhaEncriptada);
-        var usuario = mapper.toUsuario(dadosUsuarioEncriptado);
 
+        var usuario = mapper.toUsuario(dadosUsuarioEncriptado);
         usuarioRepository.save(usuario);
 
+    }
+
+    private void validandoSeUsuarioJaCadastrada(String login) {
+        boolean usuarioExistente = usuarioRepository.existsByLogin(login);
+        if (usuarioExistente) {
+            throw new ValidationException("email ou senha já existente !");
+        }
     }
 }
