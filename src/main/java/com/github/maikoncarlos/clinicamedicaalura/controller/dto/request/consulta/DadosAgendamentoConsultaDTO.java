f8bfdb23779f3270897c0f52c030f18de9c131a5
Maikoncarlos <maikonsaturno@gmail.com>
2024-02-18 22:00:44 -0300

    inclusão do @JsanAlias para poder receber diferente nomes para o mesmo atributo

--- src/main/java/com/github/maikoncarlos/clinicamedicaalura/controller/dto/request/consulta/DadosAgendamentoConsultaDTO.java
+++ src/main/java/com/github/maikoncarlos/clinicamedicaalura/controller/dto/request/consulta/DadosAgendamentoConsultaDTO.java
@@ -1,13 +1,16 @@
 package com.github.maikoncarlos.clinicamedicaalura.controller.dto.request.consulta;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record DadosAgendamentoConsultaDTO(
+        @JsonAlias("medico_id")
         Long idMedico,
         @NotNull
+        @JsonAlias("paciente_id")
         Long idPaciente,
         @NotNull
         @Future
